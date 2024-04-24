107cfebcc075ee3a58c3d148b2a6d18fee001b4f
Greg Plumbly <greg.plumbly@gmail.com>
2024-04-24 22:35:03 +0100

    list blogs, add new blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "blog_posts#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 end
