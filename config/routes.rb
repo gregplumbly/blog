b2b53ed3bf275d679930a0491bfcb92f15e1d2cb
Greg Plumbly <greg.plumbly@gmail.com>
2024-05-04 16:34:30 +0100

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root "blog_posts#index"
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  resources :blog_posts
+
+  root "blog_posts#index"
 end
