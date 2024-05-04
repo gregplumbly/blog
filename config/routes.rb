87fa920b3066d5b70d49a05da2982722e6853cd6
Greg Plumbly <greg.plumbly@gmail.com>
2024-05-04 13:43:18 +0100

    edit blog posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "blog_posts#index"
   get "up" => "rails/health#show", as: :rails_health_check
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 end
